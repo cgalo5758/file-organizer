435391568868e67a3fb3170afb63053911226222
cgalo5758 <christian.galo@outlook.com>
2022-03-11 20:14:23 -0500

    formatting change

---
+++ commands/organize.go
@@ -0,0 +1,20 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var organizeCmd = &cobra.Command{
+	Use:   "organize",
+	Short: "Organize a directory to specification",
+	Long:  `Organize a directory according to a given specification document`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("not implemented yet")
+	},
+}
+
+func init() {
+	filorgCmd.AddCommand(organizeCmd)
+}
